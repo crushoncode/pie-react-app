9196ce604a8321f45a111a22e6a25b83e74901c5
Serina Ko <serinako08@gmail.com>
2018-08-30 14:14:03 +1000

    filter out the pies to get the pie of the day and return it as object

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,15 +1,43 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
 class Store extends React.Component {
+  state = {
+    pieOfTheDay: null
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://pie.now.sh/stores/${this.props.storeData.id}/pies`)
+      .then((json) => {
+        const [pieOfTheDay] = json.data.filter((pie) => {
+          return pie.isPieOfTheDay ? true : false;
+        });
+        console.log(pieOfTheDay);
+
+        this.setState({
+          pieOfTheDay: pieOfTheDay
+        });
+      });
+  }
   render() {
     const { displayName, address, mobile, rating } = this.props.storeData;
+    const pieOfTheDay = this.state.pieOfTheDay;
     return (
       <React.Fragment>
-        <h2>{displayName}</h2>
+        <p>{displayName}</p>
         <p>{address}</p>
         <p>{mobile}</p>
         <p>{rating}</p>
+        {pieOfTheDay ? (
+          <React.Fragment>
+            <p>{pieOfTheDay.displayName}</p>
+            <p>{pieOfTheDay.quantity}</p>
+            <p>{pieOfTheDay.priceString}</p>
+          </React.Fragment>
+        ) : (
+          <p>No pie of the day</p>
+        )}
       </React.Fragment>
     );
   }
