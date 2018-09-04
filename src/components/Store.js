35ba704c2a106f907d65744a574b46494ae85dbc
Serina Ko <serinako08@gmail.com>
2018-09-04 17:29:07 +1000

    add like button

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import axios from 'axios';
 
 class Store extends Component {
   state = {
     pieOfTheDay: null
   };
+
+  handleLike = (pieOfTheDay) => {
+    this.props.handleLike(pieOfTheDay);
+  };
+
   componentDidMount() {
     axios
       .get(`https://pie.now.sh/stores/${this.props.storeData.id}/pies`)
@@ -12,6 +17,7 @@ class Store extends Component {
         const [pieOfTheDay] = json.data.filter((pie) => {
           return pie.isPieOfTheDay ? true : false;
         });
+
         this.setState({
           pieOfTheDay: pieOfTheDay
         });
@@ -20,25 +26,45 @@ class Store extends Component {
 
   render() {
     const { displayName, address, mobile, rating } = this.props.storeData;
-    const pieOfTheDay = this.state.pieOfTheDay;
-    console.log(pieOfTheDay);
+    const { pieOfTheDay } = this.state;
+    const likeButton = (
+      <button
+        onClick={() => {
+          this.handleLike(pieOfTheDay);
+        }}
+      >
+        Like
+      </button>
+    );
 
     return (
-      <React.Fragment>
+      <Fragment>
+        {pieOfTheDay ? (
+          <Fragment>
+            <h3 className="pieName">
+              <b>{pieOfTheDay.displayName}</b>
+            </h3>
+            <p>
+              {pieOfTheDay.priceString} {'for'} {pieOfTheDay.quantity}
+            </p>
+          </Fragment>
+        ) : (
+          <Fragment>
+            <h3 className="soldOut">
+              <b>Sold Out</b>
+            </h3>
+          </Fragment>
+        )}
         <p>{displayName}</p>
+        <p>
+          {'Quality: '}
+          {rating}
+          {'/10'}
+        </p>
         <p>{address}</p>
         <p>{mobile}</p>
-        <p>{rating}</p>
-        {pieOfTheDay ? (
-          <React.Fragment>
-            <p>{pieOfTheDay.displayName}</p>
-            <p>{pieOfTheDay.quantity}</p>
-            <p>{pieOfTheDay.priceString}</p>
-          </React.Fragment>
-        ) : (
-          <p>No pie of the day</p>
-        )}
-      </React.Fragment>
+        <p>{likeButton}</p>
+      </Fragment>
     );
   }
 }
