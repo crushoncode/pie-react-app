8aefaef6704fda56388d9a1605475653f18932c6
Serina Ko <serinako08@gmail.com>
2018-09-04 17:25:19 +1000

    execute a reducer function to get pieOfTheDay and save it in savedlist

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,56 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './Navbar';
+import Favourite from './Favourite';
 import StoreList from './StoreList';
+import './App.css';
 
 class App extends Component {
+  state = {
+    savedlist: []
+  };
+
+  handleLike = (pieOfTheDay) => {
+    const alreadyLikedCount = this.state.savedlist.reduce((accum, pie) => {
+      if (pie.id === pieOfTheDay.id) {
+        return accum + 1;
+      }
+      return accum;
+    }, 0);
+
+    if (alreadyLikedCount > 0) {
+      console.log('already liked');
+      return;
+    }
+
+    this.setState((prevState) => {
+      const list = prevState.savedlist;
+      list.push(pieOfTheDay);
+      return {
+        savedlist: list
+      };
+    }, console.log(this.state.savedlist));
+  };
+
   render() {
-    return <StoreList />;
+    const { savedlist } = this.state;
+    console.log('app', savedlist);
+    return (
+      <React.Fragment>
+        <Navbar />
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <StoreList handleLike={this.handleLike} />}
+          />
+          <Route
+            path="/Favourite"
+            render={() => <Favourite savedlist={savedlist} />}
+          />
+        </Switch>
+      </React.Fragment>
+    );
   }
 }
 
