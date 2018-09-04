11f9bd7cd47c701f1ac92b5bedd85a7bd821fa72
Serina Ko <serinako08@gmail.com>
2018-09-04 17:40:09 +1000

    map through savedlist and print out favourite pies

---
+++ src/components/Favourite.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Favourite extends Component {
+  render() {
+    console.log('savedlist', this.props.savedlist);
+    if (!this.props.savedlist) return 'no saved items';
+
+    const { savedlist } = this.props;
+
+    const savedItems = savedlist.map((item) => {
+      return (
+        <div className="savedItems">
+          <h2 className="pieName">
+            <b>{item.displayName}</b>
+          </h2>
+          <h3>
+            {item.priceString}
+            {' for '}
+            {item.quantity}
+          </h3>
+
+          <h3>I love this deal!</h3>
+        </div>
+      );
+    });
+
+    return <div>{savedItems}</div>;
+  }
+}
+
+export default Favourite;
